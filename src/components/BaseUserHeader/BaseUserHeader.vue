a303681ccc37822a6e6f89589b36bf5191475a24
Jakub Ciepły <jakubc01@gmail.com>
2020-06-09 17:21:21 +0200

    Add user header

---
+++ src/components/BaseUserHeader/BaseUserHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="base-user-header">
+    <h1 class="base-user-header__text">{{ header }}</h1>
+    <h2 class="base-user-header__subtext">{{ subHeader }}</h2>
+    <base-button class="base-user-header__button" outline white>{{ textButton }}</base-button>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'BaseUserHeader',
+  data() {
+    return {};
+  },
+  props: {
+    header: {
+      type: String,
+      required: true,
+    },
+    subHeader: {
+      type: String,
+      required: false,
+    },
+    textButton: {
+      type: String,
+      required: false,
+      default: 'Wróć',
+    },
+  },
+  methods: {},
+};
+</script>
+<style lang="scss" scoped src="./BaseUserHeader.scss" />
