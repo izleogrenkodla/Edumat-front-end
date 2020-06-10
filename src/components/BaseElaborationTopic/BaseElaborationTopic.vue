8301c5528f4de87c435b6ae6564055aa34f96b67
Jakub Ciepły <jakubc01@gmail.com>
2020-06-10 11:58:26 +0200

    Delete topic level

--- src/components/BaseElaborationTopic/BaseElaborationTopic.vue
+++ src/components/BaseElaborationTopic/BaseElaborationTopic.vue
@@ -7,9 +7,6 @@
     />
     <div class="base-elaboration-topic__text">
       <p class="base-elaboration-topic__text__name">{{ topic.name }}</p>
-      <p class="base-elaboration-topic__text__level" :class="levelClass">
-        {{ topic.level }}
-      </p>
     </div>
   </div>
 </template>
@@ -21,23 +18,6 @@ export default {
     topic: {
       type: Object,
       required: true,
-    },
-  },
-  computed: {
-    levelClass() {
-      if (this.topic.level.includes('Materiał na')) {
-        return 'base-elaboration-topic__text__level--extra';
-      }
-      switch (this.topic.level) {
-        case 'Szkoła podstawowa':
-          return 'base-elaboration-topic__text__level--primary-school';
-        case 'Matura podstawowa':
-          return 'base-elaboration-topic__text__level--basic';
-        case 'Matura rozszerzona':
-          return 'base-elaboration-topic__text__level--extended';
-        default:
-          return 'base-elaboration-topic__text__level';
-      }
     },
   },
 };
